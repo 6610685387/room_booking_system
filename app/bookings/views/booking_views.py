af6e2dc7065d7d313d5bc88767778925fdb064b0
Ultimata Sangrungruang <6610685387@cn102>
2026-04-22 00:56:44 +0700

    set new docker

--- app/bookings/views/booking_views.py
+++ app/bookings/views/booking_views.py
@@ -3,17 +3,24 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+
 # BookingViewSet will be implemented in Phase 4
 class BookingViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
-    
+
     def create(self, request):
-        return Response({"message": "Not implemented yet"}, status=status.HTTP_501_NOT_IMPLEMENTED)
+        return Response(
+            {"message": "Not implemented yet"}, status=status.HTTP_501_NOT_IMPLEMENTED
+        )
 
-    @action(detail=False, methods=['get'], url_path='my-bookings')
+    @action(detail=False, methods=["get"], url_path="my-bookings")
     def my_bookings(self, request):
-        return Response({"message": "Not implemented yet"}, status=status.HTTP_501_NOT_IMPLEMENTED)
+        return Response(
+            {"message": "Not implemented yet"}, status=status.HTTP_501_NOT_IMPLEMENTED
+        )
 
-    @action(detail=True, methods=['patch'])
+    @action(detail=True, methods=["patch"])
     def cancel(self, request, pk=None):
-        return Response({"message": "Not implemented yet"}, status=status.HTTP_501_NOT_IMPLEMENTED)
+        return Response(
+            {"message": "Not implemented yet"}, status=status.HTTP_501_NOT_IMPLEMENTED
+        )
--- bookings/urls.py
+++
@@ -1,4 +0,0 @@
-from django.urls import path
-from .views.booking_views import BookingViewSet
-
-urlpatterns = []
