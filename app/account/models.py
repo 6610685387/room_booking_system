0e0998696088f324a56a7af14a40d0e9da3d6968
Siranat P <6610685353@student.tu.ac.th>
2026-05-04 23:28:25 +0700

    set default user's role

--- app/account/models.py
+++ app/account/models.py
@@ -41,7 +41,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     role = models.CharField(
         max_length=20,
         choices=Role.choices,
-        default=Role.STUDENT,
+        default=Role.LECTURER,
         help_text="User's role in the system"
     )
     is_active = models.BooleanField(default=True)
